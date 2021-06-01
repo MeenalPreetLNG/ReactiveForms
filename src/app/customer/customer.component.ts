08ea4e497f9e038f9c96343205a2bbde668ecacc
Nagalakshmi <mahimamr@Mahimas-MacBook-Air.local>
2021-06-01 16:17:34 +0530

    validation changes
    validation changes

--- src/app/customer/customer.component.ts
+++ src/app/customer/customer.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Customer } from './customer';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup ,FormBuilder, Validators} from '@angular/forms';
 
 
 @Component({
@@ -12,19 +12,43 @@ export class CustomerComponent implements OnInit {
   customerForm!: FormGroup;
 //  customer = new Customer();
 
-  constructor() { }
+  constructor(private fb : FormBuilder) { }
 
   ngOnInit(): void {
-    this.customerForm =  new FormGroup({
-      firstName: new FormControl(),
-      lastName: new FormControl(),
-      email: new FormControl(),
-      sendCatalog: new FormControl(true)
+    this.customerForm = this.fb.group({
+      firstName: ['', [Validators.required, Validators.minLength(3)]],
+      lastName: ['', [Validators.required, Validators.maxLength(50)]],
+      email :['',[Validators.required,Validators.email]],
+      phone:'',
+      notification :'email',
+      rating :null,
+      sendCatalog:true
     });
+    
   }
 
   save(): void {
     console.log(this.customerForm);
     console.log('Saved: ' + JSON.stringify(this.customerForm?.value));
   }
+
+  populateTestData() :void{
+    this.customerForm.patchValue({
+      firstName :'mahima',
+      lastName :'mr',
+      // email :'mahima.mr@gmail.com',
+      sendCatalog:true
+    });
+  }
+
+  setNotification(notifyVia: string): void {
+    const phoneControl = this.customerForm.get('phone');
+    if (notifyVia === 'text') {
+      phoneControl?.setValidators(Validators.required);
+    } else {
+      phoneControl?.clearValidators();
+    }
+    phoneControl?.updateValueAndValidity();
+  }
+
 }
