15a6fd6b5f769e81b97f7b14ad87c31ce87343e3
MeenalKaurLNG <meenalpreet.kaur@lngconsultancy.co.in>
2021-06-03 11:10:44 +0530

    Add file

--- src/app/customer/customer.component.ts
+++ src/app/customer/customer.component.ts
@@ -1,7 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-import { Customer } from './customer';
-import { FormGroup ,FormBuilder, Validators} from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, AbstractControl, FormArray } from '@angular/forms';
+import { debounceTime } from 'rxjs/operators';
 
+function emailMatchers(c: AbstractControl): {[key: string]: boolean} | null {
+  const emailControl = c.get('email');
+  const confirmControl = c.get('confirmEmail');
+
+  if(emailControl?.pristine || confirmControl?.pristine){
+    return null;
+  }
+
+  if(emailControl?.value === confirmControl?.value){
+    return null;
+  }
+
+  return { 'match': true };
+}
 
 @Component({
   selector: 'app-customer',
@@ -10,21 +24,55 @@ import { FormGroup ,FormBuilder, Validators} from '@angular/forms';
 })
 export class CustomerComponent implements OnInit {
   customerForm!: FormGroup;
+  emailMessage: string = "";
 //  customer = new Customer();
 
-  constructor(private fb : FormBuilder) { }
+private validationMessages : {[key:string]: string}  = {
+  required: 'Please enter your email address.',
+  email: 'Please enter a valid email address.'
+};
+
+get addresses(): FormArray{
+  return <FormArray>this.customerForm.get('addresses');
+}
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
-    this.customerForm = this.fb.group({
-      firstName: ['', [Validators.required, Validators.minLength(3)]],
-      lastName: ['', [Validators.required, Validators.maxLength(50)]],
-      email :['',[Validators.required,Validators.email]],
-      phone:'',
-      notification :'email',
-      rating :null,
-      sendCatalog:true
+    this.customerForm =  this.fb.group({
+      firstName: ['',[
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(50)
+      ]],
+      lastName: ['', [
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(50)
+      ]],
+      emailGroup: this.fb.group({
+        email: ['',[
+          Validators.required,
+          Validators.email]],
+          confirmEmail: ['',Validators.required]
+      }, {validator: emailMatchers}),
+      sendCatalog: false,
+      phone: '',
+      notification: 'email',
+      addresses: this.fb.array([ this.buildAddress()])
     });
-    
+
+    this.customerForm.get('notification')?.valueChanges.subscribe(   
+      value => this.setNotification(value)
+    );
+
+
+    const emailControl =  this.customerForm.get('emailGroup.email');
+    emailControl?.valueChanges.pipe(
+      debounceTime(1000)
+    ).subscribe(
+      value => this.setMessage(emailControl)
+    );
   }
 
   save(): void {
@@ -32,23 +80,49 @@ export class CustomerComponent implements OnInit {
     console.log('Saved: ' + JSON.stringify(this.customerForm?.value));
   }
 
-  populateTestData() :void{
+  populateTestData(){
     this.customerForm.patchValue({
-      firstName :'mahima',
-      lastName :'mr',
-      // email :'mahima.mr@gmail.com',
-      sendCatalog:true
-    });
+      firstName: 'Jack',
+      lastName: 'Harness',
+   //   email: 'jack@torchwood.com',
+      sendCatalog: false
+    })
   }
 
-  setNotification(notifyVia: string): void {
+
+  setNotification(notifyVia: string): void{
+    console.log("Notify Via : " + notifyVia  )
     const phoneControl = this.customerForm.get('phone');
-    if (notifyVia === 'text') {
+    if(notifyVia === 'text'){
       phoneControl?.setValidators(Validators.required);
-    } else {
+    }else{
       phoneControl?.clearValidators();
     }
+
     phoneControl?.updateValueAndValidity();
+  }
+
+  setMessage(c: AbstractControl): void{
+    this.emailMessage = '';
+    if((c.touched || c.dirty) && c.errors){
+      this.emailMessage = Object.keys(c.errors).map(
+        key => this.validationMessages[key]).join(' ');
+    }
+  }
+
+  buildAddress(): FormGroup{
+    return this.fb.group({
+      addressType: 'home',
+      street1: '',
+      street2: '',
+      city: '',
+      state: '',
+      zip: ''
+    })
+  }
+
+  addAddress(): void{
+    this.addresses.push(this.buildAddress());
   }
 
 }
